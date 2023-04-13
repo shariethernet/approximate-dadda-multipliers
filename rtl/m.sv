0c839445d68afeba1c833970edcfbea99ff1427f
shariethernet <shariethernet@gmail.com>
2023-04-13 16:27:26 -0500

    Update CLA

--- rtl/m.sv
+++ rtl/m.sv
@@ -3,17 +3,17 @@ interface if_mfa_cla#(parameter WIDTH = 4) (
     input logic [WIDTH-1:0] in2,
     input logic czero,
     output logic [WIDTH-1:0] sum,
-    output logic cout
+    output logic gpr,
+    output logic ggn
 );
     logic [WIDTH-1:0] pr;
     logic [WIDTH-1:0] gn;
     logic [WIDTH-2:0] cin;
-    logic gpr;
-    logic ggn;
+
 
     modport mfa_side(
         input in1, in2, czero, cin,
-        output cout, sum, pr, gn
+        output sum, pr, gn
     );
 
     modport cla_side(
@@ -34,6 +34,23 @@ interface if_multiplier#(parameter WIDTH = 8)();
         output overflow, out
     );
 
+endinterface
+
+interface if_cla_adder#(parameter WIDTH = 16)();
+    logic [WIDTH-1:0] in1, in2;
+    logic czero;
+    logic [WIDTH-1:0] sum;
+    logic cout;
+
+    modport dut_side(
+        input in1, in2, czero,
+        output sum, cout
+    );
+
+    modport tb_side(
+        input in1, in2, czero,
+        input sum, cout
+    );
 endinterface
 /*
 // Partial Product Generation - CHECK THIS
@@ -69,6 +86,19 @@ module cla_4(if_mfa_cla.cla_side clif);
     assign clif.cin[2] = clif.gn[2] | ( clif.pr[2] & (clif.gn[1] | ( clif.pr[1] & (clif.gn[0] | ( clif.pr[0] & clif.czero))) ));
 endmodule
 
+
+module f_cla_4(
+    input logic [3:0]gn,pr,
+    input logic czero,
+    output logic [2:0] cin, 
+    output gpr, ggn
+);
+        assign gpr = & (pr) ;
+        assign ggn = gn[3] | (pr[3] & gn[2]) | (pr[3] & pr[2] & gn[1]) | (pr[3] & pr[2] & pr[1] & gn[0]); 
+        assign cin[0] = gn[0] | ( pr[0] & czero);
+        assign cin[1] = gn[1] | ( pr[1] & (gn[0] | ( pr[0] & czero)));
+        assign cin[2] = gn[2] | ( pr[2] & (gn[1] | ( pr[1] & (gn[0] | ( pr[0] & czero))) ));
+endmodule
 // Modified WIDTH-bit Full Adder 
 module mfa#(parameter WIDTH = 4)(if_mfa_cla.mfa_side mif);
 genvar i;
@@ -81,6 +111,36 @@ for(i = 0; i<WIDTH; i=i+1) begin
         assign mif.sum[i] = mif.in1[i] ^ mif.in2[i] ^ mif.cin[i-1];
     end
 end
+endmodule
+
+
+module f_cla_16#(parameter WIDTH=16)(if_cla_adder.dut_side fclif);
+    logic [(WIDTH/4)-1:0] gpr, ggn;
+    logic [(WIDTH/4)-2:0] icout;
+    logic fgpr, fggn;
+
+    if_mfa_cla#(.WIDTH(4)) if_mfa_cla_inst1(.in1(fclif.in1[3:0]), .in2(fclif.in2[3:0]),.czero(fclif.czero), .sum(fclif.sum[3:0]),.gpr(gpr[0]),.ggn(ggn[0]) );
+    //$display("\n sum[3:0] = %b",fclif.sum[3:0]);
+    cla_4 cla_inst1(.clif(if_mfa_cla_inst1));
+    mfa#(.WIDTH(4)) mfa_inst1(.mif(if_mfa_cla_inst1));
+
+    if_mfa_cla#(.WIDTH(4)) if_mfa_cla_inst2(.in1(fclif.in1[7:4]), .in2(fclif.in2[7:4]),.czero(icout[0]), .sum(fclif.sum[7:4]),.gpr(gpr[1]),.ggn(ggn[1]) );
+    cla_4 cla_inst2(.clif(if_mfa_cla_inst2));
+    mfa#(.WIDTH(4)) mfa_inst2(.mif(if_mfa_cla_inst2));
+
+    if_mfa_cla#(.WIDTH(4)) if_mfa_cla_inst3(.in1(fclif.in1[11:8]), .in2(fclif.in2[11:8]),.czero(icout[1]), .sum(fclif.sum[11:8]),.gpr(gpr[2]),.ggn(ggn[2]) );
+    cla_4 cla_inst3(.clif(if_mfa_cla_inst3));
+    mfa#(.WIDTH(4)) mfa_inst3(.mif(if_mfa_cla_inst3));
+
+    if_mfa_cla#(.WIDTH(4)) if_mfa_cla_inst4(.in1(fclif.in1[15:12]), .in2(fclif.in2[15:12]),.czero(icout[2]), .sum(fclif.sum[15:12]),.gpr(gpr[3]),.ggn(ggn[3]) );
+    cla_4 cla_inst4(.clif(if_mfa_cla_inst4));
+    mfa#(.WIDTH(4)) mfa_inst4(.mif(if_mfa_cla_inst4));
+
+    
+    f_cla_4 cla_inst_f( .gn(ggn),.pr(gpr),.czero(fclif.czero),.cin(icout),.gpr(fgpr),.ggn(fggn));
+
+    //mfa#(.WIDTH(4)) mfa_inst_f(.mif(if_mfa_cla_inst_f));
+    assign fclif.cout = (fclif.czero & fgpr) | fggn;
 endmodule
 
 module HA(
@@ -126,6 +186,7 @@ module dadda_8#(parameter WIDTH = 8)(if_multiplier.mul_side muif);
     //  Instantiate given HA, FA blocks to generate different stages 
     // 
 endmodule
+
 
 // module dadda_16
 
--- rtl/tb.sv
+++ rtl/tb.sv
@@ -1,21 +1,44 @@
+
 module tb;
-    parameter WIDTH = 4;
+
+    parameter WIDTH = 16;
     logic [WIDTH-1:0] in1;
     logic [WIDTH-1:0] in2;
     logic czero;
     logic [WIDTH-1:0] sum;
     logic cout;
+    /*
     if_mfa_cla#(.WIDTH(4)) if_mfa_cla_inst(.in1(in1), .in2(in2),.czero(czero), .sum(sum), .cout(cout));
 
     cla_4 cla_inst(.clif(if_mfa_cla_inst));
     mfa#(.WIDTH(4)) mfa_inst(.mif(if_mfa_cla_inst));
+*/
+
+    if_cla_adder if_cla_adder_inst();
+
+    f_cla_16 uut(.fclif(if_cla_adder_inst));
 
     initial begin 
-        in1 = 1;
-        in2 = 2;
-        czero = 0;
-        $monitor("[%0t] in1 = %d, in2 = %d, czero = %d, sum = %d, cout = %d ", $time, in1, in2, czero, sum, cout);
+        if_cla_adder_inst.in1 = 16'hffff;
+        if_cla_adder_inst.in2 = 16'hffff;
+        if_cla_adder_inst.czero = 0;
+        $monitor("[%0t] in1 = %d, in2 = %d, czero = %d, sum = %d, cout = %d ", $time, if_cla_adder_inst.in1, if_cla_adder_inst.in2, if_cla_adder_inst.czero, if_cla_adder_inst.sum, if_cla_adder_inst.cout);
+        /*
+        #10
+        if((if_cla_adder_inst.in1+if_cla_adder_inst.in2) == {if_cla_adder_inst.cout,if_cla_adder_inst.sum}) begin 
+            $display("%d+%d =%d : Test Passed",if_cla_adder_inst.in1, if_cla_adder_inst.in2,{if_cla_adder_inst.cout,if_cla_adder_inst.sum} );
+        end else begin 
+            $display("%d+%d =%d : Test Failed",if_cla_adder_inst.in1, if_cla_adder_inst.in2,{if_cla_adder_inst.cout,if_cla_adder_inst.sum} );
+        end
+            */
+    end
 
+    always begin 
+        #10
+        assert ((if_cla_adder_inst.in1+if_cla_adder_inst.in2) == {if_cla_adder_inst.cout,if_cla_adder_inst.sum} ) $display ("Test passed");
+        else $error("Test Failed\n Expected = %b, Obtained = %b",(if_cla_adder_inst.in1+if_cla_adder_inst.in2),{if_cla_adder_inst.cout,if_cla_adder_inst.sum});
+        #100
+        $finish;
     end
 
 endmodule
