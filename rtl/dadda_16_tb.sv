03f1ef2f827b23a6bdb26a9c66a65cfa3d8235ea
Shrihari <shariethernet@gmail.com>
2023-04-14 19:51:26 -0500

    Updates

--- rtl/dadda_16_tb.sv
+++ rtl/dadda_16_tb.sv
@@ -3,9 +3,9 @@
 module tb(if_multiplier.tb_side tbif);
 parameter WIDTH = 16;
 initial begin 
-    tbif.in1 = 2;
-    tbif.in2 = 4;
-    $monitor("[%0t] in1 = %d, in2 = %d, prod = %d, overflow = %d", $time,  tbif.in1, 
+    tbif.in1 = 16'hf;
+    tbif.in2 = 16'hf;
+    $monitor("[%0t] in1 = %d, in2 = %d, prod = %b, overflow = %d", $time,  tbif.in1, 
                                                     tbif.in2, tbif.out, tbif.overflow);
     $dumpfile("test1.vcd");
     $dumpvars;
